b2ea245648722cc8fe564c9fba759e1743b9a666
jinspiration <jincaopy@gmail.com>
2022-06-30 23:34:14 -0400

    select

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useCallback, useEffect } from "react";
 import {
   MdModeEdit,
+  MdOutlineDeleteForever,
   MdOutlineFileCopy,
   MdOutlineFileDownload,
+  MdOutlineFormatColorFill,
+  MdOutlineFormatShapes,
   MdOutlinePanTool,
   MdOutlinePrint,
   MdOutlineRedo,
@@ -15,39 +18,46 @@ import { BsHandIndexThumb, BsArrowsMove } from "react-icons/bs";
 import { ReactComponent as Defs } from "./assets/defs.svg";
 import { GRID, SHAPES, BOARDCOLORS, DOTCOLORS, HASROTATE } from "./meta.js";
 import useBuild, { ActionType } from "./build";
+import { smartSelect } from "./helpers";
 
 export type Dot = [string, string, number?];
 export type Board = { [key: number]: Dot };
 export type History = [Dot, number, number?];
+export type Selected = Array<[number, Dot]>;
 export enum ModeType {
-  SELECT = "select",
   EDIT = "edit",
+  SELECT = "select",
+  DROP = "drop",
+  MULTISELECT = "multiselect",
+  MULTIDROP = "multidrop",
   PAN = "pan",
 }
 const initBuild: Board = {
   123: ["circle", "yellow"],
-  227: ["circle", "purple"],
-  207: ["circle", "purple"],
-  217: ["circle", "purple"],
+  227: ["circle", "lilac"],
+  207: ["circle", "lilac"],
+  217: ["circle", "lilac"],
   27: ["circle", "green"],
   28: ["circle", "green"],
   3: ["arc", "blue-light", 2],
   4: ["arc", "blue-light", 3],
   19: ["arc", "blue-light", 1],
-  20: ["arc", "blue-light", 4],
+  20: ["arc", "blue-light", 0],
   44: ["rect", "green"],
   45: ["rect", "green"],
-  60: ["rect", "green", 1],
-  61: ["rect", "green", 1],
-  76: ["rect", "green", 1],
-  77: ["rect", "green", 1],
+  60: ["rect", "green"],
+  61: ["rect", "green"],
+  76: ["rect", "green"],
+  77: ["rect", "green"],
 };
 
 function App() {
-  const [dimension, setDimensions] = React.useState([32, 16]);
-  const [current, setCurrent] = React.useState<Dot>(["rect", "yellow-light"]);
-  const [mode, setMode] = React.useState<ModeType>(ModeType.EDIT);
+  const [dimension, setDimensions] = React.useState([16, 16]);
+  const [current, setCurrent] = React.useState<Dot>(["rect", "blue-light  "]);
+  const [mode, setMode] = React.useState<ModeType>(ModeType.SELECT);
   const [{ used, board }, dispatch] = useBuild();
+  const [selected, setSelected] = React.useState<Selected>([]);
+  const [droppable, setDroppable] = React.useState(false);
   const [usedCount, setUsedCount] = React.useState<
     Array<[string, string, number]>
   >([]);
@@ -78,6 +88,7 @@ function App() {
       switch (e.key) {
         case "r":
           rotateCurrent();
+          break;
       }
     };
     document.body.addEventListener("keydown", handleKey);
@@ -86,8 +97,11 @@ function App() {
     };
   }, []);
   useEffect(() => {
-    console.log(current);
-  }, [current]);
+    console.log(
+      mode,
+      selected.map(([i, dot]) => i)
+    );
+  }, [mode, selected]);
   useEffect(() => {
     const usedArray = Object.entries(used).map(([dot, occ]) => {
       const [shape, color] = dot.split(".");
@@ -99,11 +113,11 @@ function App() {
     setUsedCount(usedArray);
   }, [used]);
   return (
-    <div className="App">
+    <div className="App bg-cyan-300 ">
       <Defs />
       <div className="container h-screen p-4 lg:mx-auto grid grid-cols-2 md-layout">
         <svg
-          className="p-0.5"
+          className="m-1 p-2 bg-gray-100 rounded-lg "
           viewBox={`0 0 ${GRID} ${GRID}`}
           onClick={rotateCurrent}
         >
@@ -115,49 +129,56 @@ function App() {
             } ${GRID / 2}) `}
           />
         </svg>
-        <div className="flex flex-col items-start bg-yellow-300 p-1">
-          <div className="flex basis-1/2">
-            {SHAPES.map((shape) => (
-              <svg
-                key={shape}
-                className="h-full p-0.5"
-                viewBox={`0 0 ${GRID} ${GRID}`}
-                onClick={() => {
-                  setCurrent([shape, current[1]]);
-                }}
-              >
-                <use
-                  href={"#" + shape}
-                  className={`fill-lego-${current[1]} w-full h-full`}
-                />
-              </svg>
-            ))}
+        <div className=" m-1 flex justify-between rounded-lg ">
+          <div className="flex flex-col grow rounded-lg items-start bg-white p-1">
+            <div className="flex basis-1/2 max-w-full">
+              {SHAPES.map((shape) => (
+                <svg
+                  key={shape}
+                  className="h-full p-0.5"
+                  viewBox={`0 0 ${GRID} ${GRID}`}
+                  onClick={() => {
+                    setCurrent([shape, current[1]]);
+                  }}
+                >
+                  <use
+                    href={"#" + shape}
+                    className={`fill-lego-${current[1]} w-full h-full`}
+                  />
+                </svg>
+              ))}
+            </div>
+            <div className="flex basis-1/2 max-w-full ">
+              {Object.keys(DOTCOLORS).map((color) => (
+                <svg
+                  key={color}
+                  className="h-full p-0.5"
+                  viewBox={`0 0 ${GRID} ${GRID}`}
+                  onClick={() => {
+                    setCurrent([current[0], color.slice(5)]);
+                  }}
+                >
+                  <use
+                    href={current ? "#" + current[0] : "#rect"}
+                    className={`fill-${color} w-full h-full`}
+                    transform={`rotate(${
+                      current.length > 2 ? current[2]! * 90 : 0
+                    } ${GRID / 2} ${GRID / 2}) `}
+                  />
+                </svg>
+              ))}
+            </div>
           </div>
-          <div className="flex basis-1/2">
-            {Object.keys(DOTCOLORS).map((color) => (
-              <svg
-                key={color}
-                className="h-full p-0.5"
-                viewBox={`0 0 ${GRID} ${GRID}`}
-                onClick={() => {
-                  setCurrent([current[0], color.slice(5)]);
-                }}
-              >
-                <use
-                  href={current ? "#" + current[0] : "#rect"}
-                  className={`fill-${color} w-full h-full`}
-                  transform={`rotate(${
-                    current.length > 2 ? current[2]! * 90 : 0
-                  } ${GRID / 2} ${GRID / 2}) `}
-                />
-              </svg>
-            ))}
-          </div>
+          {mode === ModeType.DROP && (
+            <div className="flex mx-1 p-2 justify-center items-center bg-red-500 hover:bg-white rounded-lg">
+              <MdOutlineDeleteForever className="w-full h-full hover:fill-red-500" />
+            </div>
+          )}
         </div>
-        <div className="w-full">
-          <div className="rounded-lg bg-yellow-300 grid  grid-cols-2  justify-center items-center [&>div]:menu-div [&>div>svg]:menu-icon">
+        <div className="w-full flex flex-col gap-y-1 mt-1">
+          <div className="rounded-lg bg-lego-yellow grid  grid-cols-2  justify-center items-center [&>div]:menu-div [&>div>svg]:menu-icon">
             <div>
-              <BsArrowsMove />
+              <MdOutlineFormatShapes />
             </div>
             <div
               className={
@@ -165,31 +186,49 @@ function App() {
               }
               onClick={() => {
                 setMode(ModeType.EDIT);
+                setSelected([]);
               }}
             >
               <MdModeEdit />
             </div>
-            <div>
-              <MdOutlineSelectAll />
+            <div
+              className={
+                mode === ModeType.MULTISELECT
+                  ? "bg-red-500"
+                  : mode === ModeType.MULTIDROP
+                  ? "bg-blue-500"
+                  : ""
+              }
+            >
+              <MdOutlinePanTool
+                onClick={() => {
+                  setMode(ModeType.MULTISELECT);
+                  setSelected([]);
+                }}
+              />
             </div>
             <div>
               <BsHandIndexThumb
-                className={mode === ModeType.SELECT ? "bg-red-500" : ""}
+                className={
+                  mode === ModeType.SELECT
+                    ? "bg-red-500"
+                    : mode === ModeType.DROP
+                    ? "bg-blue-500"
+                    : ""
+                }
                 onClick={() => {
                   setMode(ModeType.SELECT);
+                  setSelected([]);
                 }}
               />
             </div>
             <div>
               <MdOutlineRotate90DegreesCcw />
             </div>
-            <div className={mode === ModeType.PAN ? "bg-red-500" : ""}>
-              <MdOutlinePanTool
-                onClick={() => {
-                  setMode(ModeType.PAN);
-                }}
-              />
+            <div>
+              <BsArrowsMove />
             </div>
+
             <div>
               <MdOutlineUndo />
             </div>
@@ -197,14 +236,14 @@ function App() {
               <MdOutlineRedo />
             </div>
           </div>
-          <div className="rounded-lg bg-red-500 grid grid-cols-2 [&>div]:menu-div [&>div>svg]:menu-icon">
+          <div className="rounded-lg bg-lego-lilac grid grid-cols-2 [&>div]:menu-div [&>div>svg]:menu-icon">
             <div>
               <MdOutlineWindow />
             </div>
 
             <div
               onClick={() => {
-                // dispatch({ type: ActionType.LOAD, payload: initBuild });
+                dispatch({ type: ActionType.LOAD, payload: initBuild });
               }}
             >
               <MdOutlineFileCopy />
@@ -216,59 +255,68 @@ function App() {
               <MdOutlinePrint />
             </div>
           </div>
-          <div className="flex flex-wrap bg-green-200 grow p-1">
-            <div className="w-full">
-              <span>OnBoard</span>
-            </div>
-            {usedCount
-              .filter((d) => d[2] > 0)
-              .map(([shape, color, occ], index) => (
-                <div
-                  key={[shape, color, occ, index].join(".")}
-                  className="basis-1/2 relative"
-                >
-                  <span className="absolute bottom-0 right-0 text-sm w-4 text-center bg-white rounded-full font-mono">
-                    {occ}
-                  </span>
-                  <svg
-                    className="basis-1/2 p-0.5"
-                    viewBox={`0 0 ${GRID} ${GRID}`}
-                    onClick={() => {
-                      setCurrent([shape, color]);
-                    }}
+          <div className="text-center rounded-lg bg-lego-green p-1">
+            <span className="w-full mt-2">OnBoard</span>
+            <div className="flex flex-wrap rounded-md bg-white ">
+              {usedCount
+                .filter((d) => d[2] > 0)
+                .map(([shape, color, occ], index) => (
+                  <div
+                    key={[shape, color, occ, index].join(".")}
+                    className="basis-1/2 relative"
                   >
-                    <use href={"#" + shape} className={`fill-lego-${color}`} />
-                  </svg>
-                </div>
-              ))}
-            <div className="w-full">
-              <span>Used</span>
+                    <span className="absolute bottom-0 right-0 text-sm w-4 text-center bg-white rounded-full font-mono">
+                      {occ}
+                    </span>
+                    <svg
+                      className="basis-1/2 p-0.5"
+                      viewBox={`0 0 ${GRID} ${GRID}`}
+                      onClick={() => {
+                        setCurrent([shape, color]);
+                      }}
+                    >
+                      <use
+                        href={"#" + shape}
+                        className={`fill-lego-${color}`}
+                      />
+                    </svg>
+                  </div>
+                ))}
             </div>
-            {usedCount
-              .filter((d) => d[2] < 1)
-              .map(([shape, color, occ], index) => (
-                <div
-                  key={[shape, color, occ, index].join(".")}
-                  className="basis-1/2 relative"
-                >
-                  <svg
-                    className="basis-1/2 p-0.5"
-                    viewBox={`0 0 ${GRID} ${GRID}`}
-                    onClick={() => {
-                      setCurrent([shape, color]);
-                    }}
+          </div>
+          <div className="text-center rounded-lg bg-lego-coral p-1">
+            <span className="w-full mt-1">Used</span>
+
+            <div className="flex flex-wrap rounded-md bg-white">
+              {usedCount
+                .filter((d) => d[2] < 1)
+                .map(([shape, color, occ], index) => (
+                  <div
+                    key={[shape, color, occ, index].join(".")}
+                    className="basis-1/2 relative"
                   >
-                    <use href={"#" + shape} className={`fill-lego-${color}`} />
-                  </svg>
-                </div>
-              ))}
+                    <svg
+                      className="basis-1/2 p-0.5"
+                      viewBox={`0 0 ${GRID} ${GRID}`}
+                      onClick={() => {
+                        setCurrent([shape, color]);
+                      }}
+                    >
+                      <use
+                        href={"#" + shape}
+                        className={`fill-lego-${color}`}
+                      />
+                    </svg>
+                  </div>
+                ))}
+            </div>
           </div>
           {/* <SidePanel used={used} current={current} setCurrent={setCurrent} /> */}
         </div>
         <svg
           id="board"
           viewBox={`0 0 ${dimension[0] * GRID} ${dimension[1] * GRID}`}
-          className={`bg-lego-blue max-h-full max-w-full`}
+          className={`bg-lego-blue rounded-md m-1 max-h-full max-w-full`}
         >
           <rect
             onClick={(e) => {
@@ -293,6 +341,7 @@ function App() {
             const x = place % dimension[0],
               y = Math.floor(place / dimension[0]);
             return (
+              // <>
               <use
                 key={_place}
                 onClick={(e) => {
@@ -304,6 +353,32 @@ function App() {
                       type: ActionType.REMOVE,
                       payload: place,
                     });
+                  } else if (mode === ModeType.SELECT) {
+                    setSelected([[place, board[place]]]);
+                    setMode(ModeType.DROP);
+                  } else if (mode === ModeType.DROP) {
+                    if (selected[0][0] === place) {
+                      setMode(ModeType.SELECT);
+                      setSelected([]);
+                    } else if (selected[0][0] in board) {
+                      setSelected([[place, board[place]]]);
+                    } else {
+                      //dispatch
+                      console.log("dispatch");
+                    }
+                  } else if (mode === ModeType.MULTISELECT) {
+                    const _select = smartSelect(board, dimension[0], place);
+                    console.log("smartSelect", place, _select);
+                    setSelected(_select);
+                    setMode(ModeType.MULTIDROP);
+                  } else if (mode === ModeType.MULTIDROP) {
+                    if (droppable) {
+                      //dispatch
+                      console.log("dispatch");
+                    } else {
+                      setMode(ModeType.MULTISELECT);
+                      setSelected([]);
+                    }
                   }
                 }}
                 href={"#" + shape}
@@ -312,6 +387,14 @@ function App() {
                   (rotate ?? 0) * 90
                 } ${GRID / 2} ${GRID / 2}) `}
               />
+              //   <text
+              //     x="25"
+              //     y="25"
+              //     transform={`translate(${x * GRID},${y * GRID})`}
+              //   >
+              //     {rotate ?? 0}
+              //   </text>
+              // </>
             );
           })}
         </svg>
--- src/meta.js
+++ src/meta.js
@@ -8,7 +8,6 @@ const DOTCOLORS = {
   "lego-blue-light": "#00BED3",
   "lego-green": "#00A850",
   "lego-yellow": "#FFCD03",
-  "lego-yellow-light": "#FAE862",
   "lego-lilac": "#BCA6D0",
   "lego-coral": "#F96C62",
   "lego-white": "#F3F1EF",
